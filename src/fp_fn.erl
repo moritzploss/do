b64c900554075acf50e4593b296cacb950b4ead4
Moritz Ploss <moritz.ploss@kivra.com>
2021-03-05 16:24:21 +0100

    Add tests for fn:sequence

--- src/fp_fn.erl
+++ src/fp_fn.erl
@@ -37,4 +37,14 @@ sequence(Iterable) when is_list(Iterable) or is_map(Iterable) ->
 %%%_* Tests ===================================================================
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
+
+sequence_test() ->
+  F1            = fun(X) -> X + 1 end,
+  F2            = fun(X) -> X + 2 end,
+  F3            = fun(X) -> X + 3 end,
+  SequencedList = sequence([F1, F2, F3]),
+  SequencedMap  = sequence(#{a => F1, b => F2, c => F3}),
+  ?assertEqual([2, 3, 4], SequencedList(1)),
+  ?assertEqual(#{a => 2, b => 3, c => 4}, SequencedMap(1)).
+
 -endif.
